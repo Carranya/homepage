ffa697470770a305413f133d68f3691feebe88ec
Carranya <k.gong82@gmail.com>
2022-09-02 16:21:43 +0200

    created page files

---
+++ project.php
@@ -0,0 +1,31 @@
+<p>In meiner Freizeit programmiere ich sehr gerne.<br>
+Schon seit ich ein Kind war, interessiere ich mich für das Programmieren.<br>             
+Bisher habe ich PYTHON, PERL, PHP, MySQL, HTML, CSS und Javascript angeeignet.             
+<br><br>            
+Klicken Sie auf den <b><u>Titel</u></b>, um die Programme herunter zu laden.<br>             
+Zum Ausführen der Programme müssen Sie zuerst <b><a href='https://www.apachefriends.org/de/index.html'>XAMMP</a></b> installieren.            
+</p>
+
+<h3>Meine aktuellen Projekte:</h3>
+
+<a href='project/voki.zip'><b><u>Voki (MySQL Version)</u></b></a><br>
+<p>Ein Programm zum Vokabular lernen.<br>   
+200 Wörter deutsch/japanisch sowie Demo Version mit 20 Wörter deutsch/englisch sind vorinstalliert.<br>
+Weitere Wörter-Pakete folgen erscheinen in Kürze.<br>                
+Zum Installieren, führen Sie bitte die Dateien installjap.php oder installengdemo.php aus.<br>
+<i>Aktuelle Version: 0.5</i>
+</p>
+                
+<br>
+
+<a href='project/kopfrechnen.zip'><b><u>Kopfrechnen</u></b></a><br>
+<p>Ein Tool zum Kopfrechnen üben für Kinder und Erwachsene.<br>
+4 Stufen einstellbar.<br>
+<i>Aktuelle Version: 1.0</i>
+</p>
+
+<br>
+
+<h3>GitHub</h3>
+
+<p>Alle Codes von meinen Projekten sind in meiner <b><a href='https://github.com/Carranya'>GitHub Seite</a></b> einsehbar.</p>
