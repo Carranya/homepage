442fb2550d62e75521f7221985885347114504cb
Carranya <k.gong82@gmail.com>
2022-09-03 22:24:58 +0200

    create contact page

---
+++ contact.php
@@ -0,0 +1,19 @@
+<div class="contact">
+<h2>Kontakt</h2>
+<img src="karin.jpg" alt="Karin Giang" width="300"></img>
+<p><b>Adresse:</b><br>
+Karin (Martin) Giang<br>
+Schützenmattstrasse 7<br>
+8302 Kloten<br></p>
+
+<p><b>Kontaktinformationen</b><br>
+Mobil: 078 / 823 67 56<br>
+E-Mail: <u><a href="mailto:m.giang82@gmail.com">m.giang82@gmail.com</a></u><br>
+LinkedIn: <u><a href="https://www.linkedin.com/in/karin-giang-0a6072247">https://www.linkedin.com/in/karin-giang-0a6072247</a></u><br>
+GitHub: <u><a href="https://github.com/Carranya">https://github.com/Carranya</a></u></p>
+
+<p><b>Sonstige Informationen</b><br>
+Geburtstagsdatum:. 15. Februar 1982<br>
+Zivilstand: verheiratet<br>
+Nationalität: Schweiz / Heimat: Wängi TG</p>
+</div>
