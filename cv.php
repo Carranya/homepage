59f78d3d4efa319337823a2111681261dca26f99
Carranya <k.gong82@gmail.com>
2022-09-03 21:04:54 +0200

    created page cv

---
+++ cv.php
@@ -0,0 +1,66 @@
+<div class="cvhead">
+    <h2>Mein Lebenslauf</h2>
+</div>
+<div class="cv">
+    <div class="chronicle">
+        <h3>Berufserfahrung</h3>
+        <p><b>Enderli Metallbau</b>, 8302 Kloten<br>
+        <i>Metallbaukonstrukteur</i><br>
+        Januar 2007 - Heute</p>
+
+        <p><b>Ernst Fischer AG</b>, 8590 Romanshorn<br>
+        <i>Metallbaukonstrukteur / Projektleiter</i><br>
+        März 2006 - Dezember 2006</p>
+
+        <p><b>PBF Fassadentechnik</b>, 9014 St. Gallen<br>
+        <i>Metallbaukonstrukteur</i><br>
+        August 2005 - Februar 2006</p>
+
+        <p><b>Hochuli Metallbau</b>, 8556 Wigoltingen<br>
+        <i>Metallbaukonstrukteur</i><br>
+        Dezember 2004 - August 2005</p>
+
+        <p><b>Gubler Imaging</b>, 8552 Felben-Wellhausen<br>   
+        <i>Fotodigitalisierer</i><br>
+        September 2004 - Dezember 2004</p>
+
+        <p><b>Hochuli Metallbau AG</b>, 8556 Wigoltingen<br>
+        <i>Metallbaukonstrukteur</i><br>
+        Juli 2003 - Juli 2004</p>
+
+        <h3>Bildungsweg</h3>
+        <p><b>Hochuli Metallbau AG</b>, 8556 Wigoltingen<br>
+        August 1999 - Juli 2003<br>
+        <i>Lehre als Metallbaukonstrukteur<br>
+        Abschluss: EFZ Metallbaukonstrukteur</i></p>
+    </div>
+
+    <div class="skills">
+        <h3>Fähigkeiten und Fertigkeiten</h3>
+        <p>Mehrjährige Erfahrung als <b>Metallbaukonstrukteur</b><br>
+        Mehrjährige Erfahrung mit <b>Auto CAD</b><br>
+        Mehrjährige Erfahrung mit <b>MS Office</b><br>
+        Erfahrung mit Datenbankprogramm <b>Qomet</b><br>
+        <br><br>
+        Erfahrung mit <b>PHP</b><br>
+        Erfahrung mit <b>MySQL</b><br>
+        Erfahrung mit <b>HTML / CSS</b><br>
+        Erfahrung mit <b>Ubuntu Linux und Suse Linux</b><br>
+        Grundkenntnisse mit <b>Javascript</b><br>
+        Grundkenntnisse mit <b>PERL</b></p>
+
+        <h3>Auszeichnungen</h3>
+        <p>Türspezialist<br>     
+        System Forster<br>             
+        Brandschutz / Einbruchschutz</p>
+
+        <h3>Sprachen</h3>
+        <p>Kantonesisch (Muttersprache)<br>
+        Mandarin chinesisch<br>
+        Deutsch<br>                                  
+        Englisch</p>
+    </div>
+</div>
+<div class="cvfoot">
+    <p><u><a href="Lebenslauf.pdf">Download als PDF-Datei</a></u></p>
+</div>
---
+++ karriere.php
@@ -0,0 +1,18 @@
+<h2>Meine Karriere</h2>
+<p>Nach der obligtorischen Grundschule und Sekundarschule hatte ich eine Lehre als Metallbaukonstrukteur (Metallbauzeichner) absolviert.
+<br><br>
+Bis jetzt arbeitete ich als Metallbaukonstrukteur.
+<br><br>
+Jedoch muss ich den Beruf als Metallbaukonstrukteur aus gesundheitlichen Gründen aufgeben.
+<br><br>
+Da ich als Metallbaukonstrukteur viel mit Computer arbeite und in der Freizeit auch sehr gerne programmiere, habe ich nach langer Ünerlegung entschieden, in die Informatik-Branche einzusteigen.
+<br><br>
+Momentan bin ich auf der Suche einer Firma, die bereit ist, mir den Einstieg in die Informatik Branche zu ermöglichen.
+Ich habe zwar bisher noch keine Ausbildung im Bereich Informatik, aber ich konnte privat durch Selbststudium das Programmieren von PERL, PHP, HTML, CSS und Javascript aneignen.
+<br><br>
+Bisher war ich in der Metallbaufirma als EDV Verantwortliche tätig.
+Software Installationen, Instandhaltung der Software (Wartungen und Updates), Einfache Support (Software, Netzwerk, Drucker), Monitoring sowie Einführungen neuer Software an den Mitarbeitern gehörten zu meiner Aufgaben. Darum kann ich mich zutrauen, eine Stelle als Level 1 IT-Supporterin anzunehmen.
+Nebenbei programmiere ich selber sehr gerne im Privat. Kleine PHP und Javascript Tools und auch Webpages. Einige meine Projekte können Sie unter "Projekte" nachlesen.</p>
+
+
+ 
