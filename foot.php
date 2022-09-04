76a953fcfb116dbe4c4632554bc92d6598b0fb29
Carranya <k.gong82@gmail.com>
2022-09-04 22:26:21 +0200

    added foot.php for contact informations

---
+++ foot.php
@@ -0,0 +1,14 @@
+<div class="footleft">
+<p>Karin (Martin) Giang<br>
+Schützenmattstrasse 7<br>
+8302 Kloten<br>
+<img src="icons/phone.png" width="15" height="15"> +41 78 823 67 56</img></p>
+</div>
+
+<div class="footright">
+<p>
+<a href="mailto:m.giang82@gmail.com"><img src="icons/mail.png" width="40" height="40"></img></a>
+<a href="https://www.linkedin.com/in/karin-giang-0a6072247"><img src="icons/linkedin.png" width="40" height="40"></a>
+<a href="https://github.com/Carranya"><img src="icons/github.png" width="40" height="40"></a>
+</p>
+</div>
