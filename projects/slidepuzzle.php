35eea8f77f744f663df8703f6edfa58726b92d54
Carranya <k.gong82@gmail.com>
2022-09-13 23:57:06 +0200

    added text for project slidepuzzle

--- projects/slidepuzzle.php
+++ projects/slidepuzzle.php
@@ -1,11 +1,10 @@
 <div style="border:1px solid black; padding:10px; margin: 10px">
-<center><a href=''><b><u>SLide Puzzle</u></b></a><br></center>
+<center><a href=''><b><u>Slide Puzzle</u></b></a><br></center>
 <br>
 <center><img src="screenshots/slidepuzzle.jpg"></img></center>
-<p>Ein Tool zum Kopfrechnen üben für Kinder und Erwachsene.<br>
-4 Stufen einstellbar.<br>
-Es wird 10 zufallsgerenderte Aufgaben generiert.<br>
-Spassige Denksportübung für Jung und Alt.<br>
+<p>Ein Browserspiel gegen Langeweile<br>
+Versuchen Sie mit möglichst wenig Züge das Bild zusammenzusetzen.<br>
+Sie können auch Ihr Lieblingsfoto für das Spiel hochladen.<br>
 <i>Aktuelle Version: 1.2</i>
 </p>
 </div>
