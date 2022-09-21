6071e63c43bbb8e766b1c3b831af5477b483b02f
Carranya <k.gong82@gmail.com>
2022-09-21 21:26:59 +0200

    update home.php

--- home.php
+++ home.php
@@ -1,23 +1,29 @@
-<div>
-    <h2>Herzlich Willkommen!</h2>
-</div>
-<div class="home">
-    <div class="homeleft">
-        Guten Tag, ich bin Karin Giang.
-        <br><br>
-        Ich bin eine Hobby Programmiererin und Webdesignerin und möchte meinen Hobby zum Beruf zu machen.
-        <br><br>
-        Momentan bin ich auf der Suche einer Firma, die bereit ist, mir den Einstieg in die Informatik Branche zu ermöglichen.
-        Ich habe zwar bisher noch keine Ausbildung im Bereich Informatik, aber ich konnte privat durch Selbststudium das Programmieren von PERL, PHP, HTML, CSS und Javascript aneignen.
-        <br><br>
-        Bisher war ich in einer Metallbaufirma als EDV Verantwortliche tätig.
-        Software Installationen, Instandhaltung der Software (Wartungen und Updates), Einfache Support (Software, Netzwerk, Drucker), Monitoring sowie Einführungen neuer Software an den Mitarbeitern gehörten zu meiner Aufgaben. Darum kann ich mir zutrauen, eine Stelle als Level 1 IT-Supporterin anzunehmen.
-        <br><br>
-        In meiner Freizeit programmiere ich sehr gerne und lerne immer neue Techniken und Programmiersprachen kennen.
-        Bisher habe ich bereits kleine PHP- und Javascript-Tools sowie auch Webpages programmiert. Einige meine Projekte können Sie unter "Projekte" nachlesen.</p>
-    </div>
+<div id="home">
 
-    <div class="homeright">
-        <img src="Karin2.jpg" width="300"></img>
-    </div>
+    <h2 id="homeTitle">Herzlich Willkommen!</h2>
+    <img src="Karin2.jpg" id="homePic"></img>
+
+    <p>
+    Guten Tag, ich bin Karin Giang.
+    <br><br>
+    Ich bin eine Hobby Programmiererin und Webdesignerin und möchte meinen Hobby zum Beruf zu machen.
+    <br><br>
+    Momentan bin ich auf der Suche einer Firma, die bereit ist,<br>
+    mir den Einstieg in die Informatik Branche zu ermöglichen.
+    Ich habe zwar bisher noch keine Ausbildung im Bereich Informatik absolviert,<br>
+    aber ich konnte privat durch Selbststudium das Programmieren von HTML, CSS, PHP, MySQL und Javascript aneignen.
+    <br><br>
+    In meiner Freizeit programmiere ich sehr gerne und lerne immer neue Techniken und Programmiersprachen kennen.
+    Bisher habe ich bereits kleine PHP- und Javascript-Tools sowie auch Webpages programmiert. Einige meine Projekte können Sie in meinen 
+    <a href="https://github.com/Carranya" target="_blank"><img src="icons/github.png" width="40" height="40"></img>-Account</a> einsehen.</p>      
+    <br><br>
+
+    <p id="homeTech">
+        <img src='icons/tech/html.png'></img>
+        <img src='icons/tech/css.png'></img>
+        <img src='icons/tech/php.png'></img>
+        <img src='icons/tech/mysql.png'></img>
+        <img src='icons/tech/javascript.png'></img>
+    </p>
+
 </div>
