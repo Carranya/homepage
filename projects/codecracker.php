67ea30d69ecd0a0645c4704f18815b3a2c487d36
Carranya <k.gong82@gmail.com>
2022-11-07 16:08:02 +0100

    deleted projects pages

--- projects/codecracker.php
+++
@@ -1,8 +0,0 @@
-<div style="border:1px solid black; padding:10px; margin: 10px">
-<center><a href=''><b><u>Code Cracker</u></b></a><br></center>
-<br>
-<center><img src="img/screenshots/codecracker.jpg"></img></center>
-<p>Ein lustiges, Mastermind-Like Browser Game für zwischendurch.<br>
-Schaffst du nach 10 Versuche den zufälligen, geheimen Code zu knacken?
-</p>
-</div>
--- projects/slidepuzzle.php
+++
@@ -1,9 +0,0 @@
-<div style="border:1px solid black; padding:10px; margin: 10px">
-<center><a href=''><b><u>Slide Puzzle</u></b></a><br></center>
-<br>
-<center><img src="img/screenshots/slidepuzzle.jpg"></img></center>
-<p>Ein Browserspiel gegen Langeweile<br>
-Versuchen Sie mit möglichst wenig Züge das Bild zusammenzusetzen.<br>
-Sie können auch Ihr Lieblingsfoto für das Spiel hochladen.
-</p>
-</div>
