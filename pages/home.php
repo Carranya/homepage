69be8c5281198b3dcc6695577cbec951ac616ab9
Carranya <k.gong82@gmail.com>
2022-11-09 10:43:25 +0100

    new home page

--- pages/home.php
+++ pages/home.php
@@ -1,25 +1,9 @@
-<div id="home">
-
+<div>
     <h2>Herzlich Willkommen!</h2>
-    <img src="img/karin2.jpg" class="showPic"></img>
-
-<p>
-Guten Tag, ich bin Karin Giang.
-<br><br>
-Ich bin eine Programmiererin und Webentwicklerin.
-<br><br>
-Momentan bin ich auf der Suche nach einer Firma, die bereit ist, 
-mir den Einstieg in die Informatik Branche zu ermöglichen.
-Ich habe zwar bisher noch keine Ausbildung im Bereich Informatik absolviert, 
-aber ich konnte privat durch Selbststudium das Programmieren von HTML, CSS, PHP, MySQL, Javascript etc. aneignen.
-Näheres dazu finden Sie auf meiner Profil-Seite (Icon rechts unten).
-<br><br>
-In meiner Freizeit programmiere ich sehr gerne und lerne immer neue Techniken und Programmiersprachen kennen.
-Bisher habe ich bereits kleine Webtools sowie auch Webpages programmiert.    
-<br><br>
-In offiziellen Dokumenten verwende ich noch den männlichen Namen Martin.
-Jedoch in der Freizeit werden meine Freunde mich immer mehr als weiblich ansprechen und ansehen.
-Momentan bin ich noch sehr beschäftigt mit der beruflichen Wandel. Sobald ich mit der neuen Branche stabilisiert habe, werde ich mit der Hormontheraphie beginnen.</p>
-<br>
-<p>Meine offizielle Webseite: <a href="http://www.karingiang.ch" target="_blank">Hier klicken</a></p>
+    <p>
+        Diese Webseite ist mein erster Versuch mit HTML, PHP und CSS. Davor arbeitete ich nur mit HTML.<br>
+        Sie ist auch meine alte Homepage. Damals hatte ich noch nicht so viel Ahnung gehabt, wie eine moderne Webseite aussieht.<br>
+        Die klassische "Leftside Navigation" sehen wir heutzutage immer weniger. Es ist auch nicht Smartphone freundlich.
+    </p>
+    <p>Meine offizielle Webseite: <a href="http://www.karingiang.ch" target="_blank">Hier klicken</a></p>
 </div>
