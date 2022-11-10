2093bc565269a64e3c45763de3e1fbff8df1dcab
Carranya <k.gong82@gmail.com>
2022-11-10 10:27:54 +0100

    added style

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 <html lang="de">
 <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-wdth, initial-scale=1.0">
     <title>Karin Giang - Webentwicklerin</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
--- pages/websites.php
+++ pages/websites.php
@@ -7,12 +7,11 @@ Am Liebsten programmiere ich Webseiten.<br>
 Es ist auch mein Hobby, immer wieder neue Ideen für Webseiten zu entwickeln und diese auf dem Bildschirm zu bringen.</p>
 <br>
 <p>Alle Codes von meiner Webseiten Vorschlägen sind in meiner <b><a href='https://github.com/Carranya'>GitHub Seite</a></b> einsehbar.</p>
-
-    <div class="websitesContent">
-        <p>
-            <a href="projects/website_proposal_1/index.php" target="_blank"><img src="img/websites/website_proposal_1.jpg" class="websitesPic"></img></a>
-            <a href="projects/website_proposal_2/index.php" target="_blank"><img src="img/websites/website_proposal_2.jpg" class="websitesPic"></img></a>
-            <a href="projects/website_proposal_3/index.php" target="_blank"><img src="img/websites/website_proposal_3.jpg" class="websitesPic"></img></a>
-        </p>
+    <div class="centerPage">
+        <div id="websitesContent">
+            <a href="projects/website_proposal_1/index.php" target="_blank"><img src="img/websites/website_proposal_1.jpg" class="websitesPic"></a>
+            <a href="projects/website_proposal_2/index.php" target="_blank"><img src="img/websites/website_proposal_2.jpg" class="websitesPic"></a>
+            <a href="projects/website_proposal_3/index.php" target="_blank"><img src="img/websites/website_proposal_3.jpg" class="websitesPic"></a>
+        </div>
     </div>
 </div>
